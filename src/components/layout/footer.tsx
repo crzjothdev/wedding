ed6b72a092499efa7f0d1a847b29e1bfa1e4a608
Angelo Cruz <developer.angelo.cruz@gmail.com>
2023-09-17 14:46:29 +0100

    Personaliced Copyrighting elimination

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,7 +5,6 @@ export default function Footer() {
     return (
         <footer className="flex flex-col p-4 md:flex-row md:px-8 md:py-[4rem] text-white dark:bg-black">
             <div className="grow flex items-center py-4 md:py-0">
-                <span>Copyright © 2023 Jeffry Zacarias</span>
             </div>
             <hr className="h-[2px] border-0 bg-gray-700 my-4" />
             <div className="flex">
